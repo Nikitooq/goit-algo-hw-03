4671e54de387892f1e6d4c840c4a11407bb06b10
Nikitoq <nikita.nikitov8546@gmail.com>
2026-01-19 16:40:29 +0100

    added try function

--- hm1.py
+++ hm1.py
@@ -2,8 +2,11 @@ from datetime import datetime
 
 def get_days_from_today(date):
     today = datetime.today()
-    date_to_string = datetime.strptime(date, "%Y-%m-%d")
+    try:
+        date_to_string = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        print("Format of the date must be: YYYY-MM-DD")
     date_difference = (today - date_to_string)
     return date_difference.days
 
-print(get_days_from_today("2010-10-09"))
+print(get_days_from_today("13.14.2000"))
