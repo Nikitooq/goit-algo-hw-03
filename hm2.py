d93759a0f6f8430dbbbd4a0277b4861bac1f01ad
Nikitoq <nikita.nikitov8546@gmail.com>
2026-01-24 20:48:52 +0100

    hh

--- hm2.py
+++ hm2.py
@@ -3,16 +3,19 @@ import random
 def get_numbers_ticket(min, max, quantity):
     random_numbers = set()
     not_valid_number = []
-    while  len(random_numbers) < quantity:
-        if max < min:
-            return not_valid_number
-        elif min < 1:
-            return not_valid_number
-        elif max > 1000:
-            return not_valid_number
-        else:
-            number = random.randint(min, max)
-            random_numbers.add(number)
-    return sorted(random_numbers)
+    try:
+        while  len(random_numbers) < quantity:
+            if max < min:
+                return not_valid_number
+            elif min < 1:
+                return not_valid_number
+            elif max > 1000:
+                return not_valid_number
+            else:    
+                number = random.randint(min, max)
+                random_numbers.add(number)
+        return sorted(random_numbers)
+    except KeyboardInterrupt:
+        return not_valid_number
 
-print(get_numbers_ticket(10, 4, 5))
+print(get_numbers_ticket(10, 14, 6))
--- hm3.py
+++ hm3.py
@@ -1,19 +1,18 @@
 import re
 
 def normalize_phone(phone_number):
-    for number in phone_number:
-        cleaned_number = re.sub(r"\D", "", number)
-        if not cleaned_number.startswith("+"):
-            if cleaned_number.startswith("380"):
-                cleaned_number = "+" + cleaned_number
-                return cleaned_number
-            else:
-                cleaned_number = "+38" + cleaned_number
+    cleaned_number = re.sub(r"\D", "", phone_number)
+    if not cleaned_number.startswith("+"):
+        if cleaned_number.startswith("380"):
+            cleaned_number = "+" + cleaned_number
+            return cleaned_number
+        else:
+            cleaned_number = "+38" + cleaned_number
     return cleaned_number
 
 
 
-raw_numbers = "380501234567",
+raw_numbers = "067\\t123 4567"
 
 sanitized_numbers = normalize_phone(raw_numbers)
 print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
