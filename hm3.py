aa770505e392dc15589b264d54209bbbfa557f78
Nikitoq <nikita.nikitov8546@gmail.com>
2026-01-25 18:04:24 +0100

    ch

--- hm3.py
+++ hm3.py
@@ -1,7 +1,11 @@
 import re
 
 def normalize_phone(phone_number):
-    cleaned_number = re.sub(r"\D", "", phone_number)
+    pattern = r"[+, 0-9]"
+    cleaned_number = re.sub(r"\s", "", phone_number)
+    cleaned_number = re.findall(pattern, cleaned_number)
+    cleaned_number = "".join(cleaned_number)
+    print(cleaned_number)
     if not cleaned_number.startswith("+"):
         if cleaned_number.startswith("380"):
             cleaned_number = "+" + cleaned_number
@@ -12,7 +16,7 @@ def normalize_phone(phone_number):
 
 
 
-raw_numbers = "067\\t123 4567"
+raw_numbers = "+432 10 123 45 67"
 
 sanitized_numbers = normalize_phone(raw_numbers)
 print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
