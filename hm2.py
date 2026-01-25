e7aff142694814ca02a67bc45b9e43b2a7445f5e
Nikitoq <nikita.nikitov8546@gmail.com>
2026-01-25 17:16:51 +0100

    removed input validation conditions from the "while"

--- hm2.py
+++ hm2.py
@@ -4,17 +4,17 @@ def get_numbers_ticket(min, max, quantity):
     random_numbers = set()
     not_valid_number = []
     try:
-        while  len(random_numbers) < quantity:
-            if max < min:
-                return not_valid_number
-            elif min < 1:
-                return not_valid_number
-            elif max > 1000:
-                return not_valid_number
-            else:    
+        if max < min:
+            return not_valid_number
+        elif min < 1:
+            return not_valid_number
+        elif max > 1000:
+            return not_valid_number
+        else:
+            while  len(random_numbers) < quantity:    
                 number = random.randint(min, max)
                 random_numbers.add(number)
-        return sorted(random_numbers)
+            return sorted(random_numbers)
     except KeyboardInterrupt:
         return not_valid_number
 
